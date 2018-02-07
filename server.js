b840ece0c875feac590d37d6e005dda29579796d
Trey Hoover <treyhoover@gmail.com>
2018-02-07 14:27:44 -0500

    Use express instead of next-routes

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-// This file doesn't go through babel or webpack transformation.
-// Make sure the syntax and sources this file requires are compatible with the current node version you are running
-// See https://github.com/zeit/next.js/issues/1245 for discussions on Universal Webpack or universal Babel
-require('dotenv').config();
-
-const { createServer } = require('http');
+const express = require('express');
 const next = require('next');
-const routes = require('./routes');
 
 const port = parseInt(process.env.PORT, 10) || 8080;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const handler = routes.getRequestHandler(app);
+const handle = app.getRequestHandler();
 
-(async () => {
-  await app.prepare();
-  const server = createServer(handler);
+app.prepare()
+  .then(() => {
+    const server = express();
 
-  server.listen(port, (err) => {
-    if (err) throw err;
+    server.get('/', (req, res) => {
+      return res.redirect("/feelz/new");
+    });
 
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})();
+    server.get('/feelz/new', (req, res) => {
+      return app.render(req, res, '/feelz/new');
+    });
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    });
+
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  });
